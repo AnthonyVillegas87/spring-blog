0c1563647d06092193dc03fa7d2cd185ccd42bf2
Anthony Villegas <villegasanthony87@yahoo.com>
2021-11-02 15:52:40 -0500

    feat: thymeleaf tag lecture notes

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,9 +1,13 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Coffee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CoffeeController {
@@ -15,12 +19,30 @@ public class CoffeeController {
 
     @GetMapping("/coffee/{roast}")
     public String roastSelection(@PathVariable String roast, Model model) {
-        model.addAttribute("roast", roast);
-        boolean darkChoice = false;
-        if(roast.equals("dark")) {
-            darkChoice = true;
+        Coffee selection = new Coffee(roast, "Cool Beans");
+        Coffee selection2 = new Coffee(roast, "Java Beans");
+        selection.setRoast(roast);
+        if (roast.equals("dark")) {
+            selection.setOrigin("Columbia");
+            selection2.setOrigin("Brazil");
+        } else if (roast.equals("medium")) {
+            selection.setOrigin("New Guinea");
+            selection2.setOrigin("Sumatra");
+        } else {
+            selection.setOrigin("Kenya");
+            selection2.setOrigin("Ethiopia");
         }
-        model.addAttribute("darkChoice", darkChoice);
+//        model.addAttribute("roast", roast);
+//        boolean darkChoice = false;
+//        if(roast.equals("dark")) {
+//            darkChoice = true;
+//        }
+        List<Coffee> selections = new ArrayList<>();
+        selections.add(selection);
+        selections.add(selection2);
+        model.addAttribute("roast", roast);
+        model.addAttribute("selections", selections);
+
         return "views-lecture/coffee";
     }
 
