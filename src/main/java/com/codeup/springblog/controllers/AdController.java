d1607016d48e1752e1a4150daed1f5c76cf765d4
Anthony Villegas <villegasanthony87@yahoo.com>
2021-11-02 11:00:11 -0500

    feat: pt 2 of spring lecture

--- src/main/java/com/codeup/springblog/controllers/AdController.java
+++ src/main/java/com/codeup/springblog/controllers/AdController.java
@@ -1,10 +1,7 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AdController {
@@ -24,6 +21,24 @@ public class AdController {
     @ResponseBody
     public String createAds() {
         return "Create Ads";
+    }
+
+    @GetMapping("/color/{color}")
+    @ResponseBody
+    public String returnColor(@PathVariable String color) {
+        return "<h1 style='color:" + color + "'>" + color + "</h1>";
+    }
+
+    @GetMapping("/bg/{color1}/font/{color2}")
+    @ResponseBody
+    public String returnBgFontColor(@PathVariable String color1, @PathVariable String color2) {
+        return String.format("<h1>style='background-color: %s;color:%s'>THIS IS THE FONT</h1>", color1,color2);
+    }
+
+    @GetMapping("/name")
+    @ResponseBody
+    public String returnName(@RequestParam String n) {
+        return "Returning Name: " + n;
     }
 
 
