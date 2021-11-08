074e76e5344681f080e0f78ca3c36cff4e4419f2
Anthony Villegas <villegasanthony87@yahoo.com>
2021-11-08 10:30:00 -0600

    feat: form model binding lecture

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,16 +1,24 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Coffee;
+import com.codeup.springblog.repositories.CoffeeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CoffeeController {
+
+    private final CoffeeRepository coffeeRepository;
+
+    public CoffeeController(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+    }
+
+
 
     @GetMapping("/coffee")
     public String coffeeInfo() {
@@ -19,31 +27,47 @@ public class CoffeeController {
 
     @GetMapping("/coffee/{roast}")
     public String roastSelection(@PathVariable String roast, Model model) {
-        Coffee selection = new Coffee(roast, "Cool Beans");
-        Coffee selection2 = new Coffee(roast, "Java Beans");
-        selection.setRoast(roast);
-        if (roast.equals("dark")) {
-            selection.setOrigin("Columbia");
-            selection2.setOrigin("Brazil");
-        } else if (roast.equals("medium")) {
-            selection.setOrigin("New Guinea");
-            selection2.setOrigin("Sumatra");
-        } else {
-            selection.setOrigin("Kenya");
-            selection2.setOrigin("Ethiopia");
-        }
-//        model.addAttribute("roast", roast);
-//        boolean darkChoice = false;
-//        if(roast.equals("dark")) {
-//            darkChoice = true;
+
+        model.addAttribute("selections", coffeeRepository.findByRoast(roast));
+
+//        Coffee selection = new Coffee(roast, "Cool Beans");
+//        Coffee selection2 = new Coffee(roast, "Java Beans");
+//        selection.setRoast(roast);
+//        if (roast.equals("dark")) {
+//            selection.setOrigin("Columbia");
+//            selection2.setOrigin("Brazil");
+//        } else if (roast.equals("medium")) {
+//            selection.setOrigin("New Guinea");
+//            selection2.setOrigin("Sumatra");
+//        } else {
+//            selection.setOrigin("Kenya");
+//            selection2.setOrigin("Ethiopia");
 //        }
-        List<Coffee> selections = new ArrayList<>();
-        selections.add(selection);
-        selections.add(selection2);
-        model.addAttribute("roast", roast);
-        model.addAttribute("selections", selections);
+////        model.addAttribute("roast", roast);
+////        boolean darkChoice = false;
+////        if(roast.equals("dark")) {
+////            darkChoice = true;
+////        }
+//        List<Coffee> selections = new ArrayList<>();
+//        selections.add(selection);
+//        selections.add(selection2);
+//        model.addAttribute("roast", roast);
+//        model.addAttribute("selections", selections);
 
         return "views-lecture/coffee";
+    }
+
+
+    @GetMapping("/coffee/createCoffee")
+    public String showCoffeeForm(Model model) {
+        model.addAttribute("coffee", new Coffee());
+        return "/coffees/createCoffee";
+    }
+
+    @PostMapping("/coffee/createCoffee")
+    public String createCoffee(@ModelAttribute Coffee coffee) {
+        coffeeRepository.save(coffee);
+        return "redirect:/coffee";
     }
 
 }
--- src/main/java/com/codeup/springblog/models/Coffee.java
+++ src/main/java/com/codeup/springblog/models/Coffee.java
@@ -1,8 +1,22 @@
 package com.codeup.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "coffees")
 public class Coffee {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 6)
     private String roast;
+    @Column(nullable = false, length = 50)
     private String origin;
+    @Column(nullable = false, length = 50)
     private String brand;
 
 
@@ -13,6 +27,21 @@ public class Coffee {
    public Coffee (String roast, String brand) {
       this.roast = roast;
       this.brand = brand;
+    }
+
+    public Coffee(String brand, String origin, String roast) {
+        this.brand = brand;
+        this.origin = origin;
+        this.roast = roast;
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getOrigin() {
