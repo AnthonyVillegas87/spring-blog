323a554e71242bf862e3019299d53a40e13cda44
Anthony Villegas <villegasanthony87@yahoo.com>
2021-11-09 14:34:38 -0600

    feat: Incorporated sign-up & log-out links to my partials

--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -1,4 +1,37 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.User;
+import com.codeup.springblog.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+        this.userRepository = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "posts/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+
 }
